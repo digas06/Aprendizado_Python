d94326d0146262f335745e7604e9c7d897691eb4
Diego Mateus <diegomm.200529@gmail.com>
2023-09-06 10:52:37 -0300

    ex21_32
    Adicionei o exercício 21_
    Adicionei o exercício 32_

--- Modulo01/Fundamentos/32.py
+++ Modulo01/Fundamentos/32.py
@@ -1,1 +1,11 @@
-''''''
+'''Faça um programa que leia um ano qualquer e mostre se ele é bissexto.'''
+import datetime
+
+ano = int(input('Digite um ano (coloque 0 para analizar o ano atual): '))
+
+if ano == 0:
+    ano = datetime.date.today().year
+if ano % 4 == 0:
+    print(f'O ano {ano} é bissexto.')
+else:
+    print(f'O ano {ano} não é bissexto.')
